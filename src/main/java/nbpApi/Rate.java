37ca3337fdbf24a17ed4a8f302ca3ee8a3bb7f9e
Kacper Kochanowski <kochanowski.k97@gmail.com>
2022-02-14 22:23:09 +0100

    First part of project

---
+++ src/main/java/nbpApi/Rate.java
@@ -0,0 +1,20 @@
+package nbpApi;
+
+public class Rate {
+    private String currency;
+    private String code;
+    private double mid;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public double getMid() {
+        return mid;
+    }
+
+}
